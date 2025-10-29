b672802a4c61089ef033384a77eb44c7a2b101c5
DELL <vivinprakash2005@gmail.com>
2025-10-29 16:00:32 +0530

    Initial Commit

---
+++ Prob_215.java
@@ -0,0 +1,15 @@
+
+import java.util.PriorityQueue;
+
+class Prob_no_215 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
