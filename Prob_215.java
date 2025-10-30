2c7920a850599b5e1a9680f0ddf4d85148a54306
DELL <vivinprakash2005@gmail.com>
2025-10-30 16:33:07 +0530

    Initial commit

--- Prob_215.java
+++ Prob_215.java
@@ -1,7 +1,7 @@
 
 import java.util.PriorityQueue;
 
-class Prob_no_215 {
+class Prob_215{
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int num : nums) {
---
+++ Prob_no.148.java
@@ -0,0 +1,36 @@
+class Prob_no_148 {
+    public ListNode sortList(ListNode head) {
+         if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return merge(left, right);
+    }
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 != null) tail.next = l1;
+        if (l2 != null) tail.next = l2;
+
+        return dummy.next;
+    }
+}
